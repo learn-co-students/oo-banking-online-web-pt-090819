0fc0bc3135ccc668ddee65b575e76c23b71bb5cd
Cody Dupuis <ctdupuis7@gmail.com>
2019-10-03 01:43:11 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,17 +13,21 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
+    if @sender.balance > @amount && @status == "pending" && valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    else
+    else 
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
   
   def reverse_transfer
-    
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
   end
 end
