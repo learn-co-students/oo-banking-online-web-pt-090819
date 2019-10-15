1594e7211071612faf09c48aae9e8c237701f3f2
Amgad Malik <amgadmalik@yahoo.com>
2019-10-15 00:42:06 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
-
+  attr_accessor :status, :balance
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    save
+  end
+  
+  def save
+    @@all << self
+  end 
+  
+  def deposit(deposit)
+    @balance += deposit
+  end 
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end 
+  
+  def valid?
+    status == "open" && balance > 0
+  end 
+  
+  def close_account
+    self.status = "closed"
+  end 
 end
